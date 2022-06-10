59b70fc71f86b80d15327b7901f3ffa7b9e97c69
Alex <andres.al.campos@gmail.com>
2022-06-10 16:41:52 -0700

    passing all props at once with destructuring

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -7,15 +7,7 @@ function App() {
     <div>
       <h1 className="heading">My Contacts</h1>
       {contacts.map((props) => {
-        return (
-          <Card
-            key={props.id}
-            name={props.name}
-            imgURL={props.imgURL}
-            phone={props.phone}
-            email={props.email}
-          />
-        );
+        return <Card key={props.id} {...props} />;
       })}
     </div>
   );
