b8c5677da68ba4f3e0ec0b3a9e03113bffb11044
Alex <andres.al.campos@gmail.com>
2022-06-10 16:53:42 -0700

    adding the rest of the styles

--- src/components/Avatar.jsx
+++ src/components/Avatar.jsx
@@ -1,4 +1,6 @@
 import React from "react";
+import "../../public/styles.css";
+
 export default function Avatar(props) {
-  return <img src={props.imgURL} alt="avatar_img" />;
+  return <img className="circle-img" src={props.src} alt="avatar_img" />;
 }
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,15 +1,17 @@
 import React from "react";
 import Avatar from "./Avatar.jsx";
+import "../../public/styles.css";
+
 export default function Card(props) {
   return (
     <div className="card">
       <div className="top">
-        <h2>{props.name}</h2>
-        <Avatar imgURL={props.imgURL} />
+        <h2 className="name">{props.name}</h2>
+        <Avatar src={props.imgURL} />
       </div>
       <div className="bottom">
-        <p>{props.phone}</p>
-        <p>{props.email}</p>
+        <p className="info">{props.phone}</p>
+        <p className="info">{props.email}</p>
       </div>
     </div>
   );
